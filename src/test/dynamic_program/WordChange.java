b5c7999cf06c221577f6928fd06f70b590350c1e
张颖 <zhangying54@meituan.com>
2020-08-04 22:56:56 +0800

    更新动归相关

---
+++ src/test/dynamic_program/WordChange.java
@@ -0,0 +1,31 @@
+package test.dynamic_program;
+
+
+/**
+ * 给定两个单词 word1 和 word2，计算出将 word1 转换成 word2 所使用的最少操作数 。
+ * 你可以对一个单词进行如下三种操作：
+ * 插入一个字符 删除一个字符 替换一个字符
+ *
+ * 示例：
+ * 输入: word1 = "horse", word2 = "ros"
+ * 输出: 3
+ * 解释:
+ * horse -> rorse (将 'h' 替换为 'r')
+ * rorse -> rose (删除 'r')
+ * rose -> ros (删除 'e')
+ */
+public class WordChange {
+
+
+    /**
+     * 返回最少操作数
+     *
+     * @param s1 单词s1
+     * @param s2 单词s2
+     * @return
+     */
+    public int solution(String s1, String s2) {
+
+        return 0;
+    }
+}
